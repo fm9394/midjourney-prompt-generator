fc111c7834ed48a8f426f96548c27453994aa622
Amery2010 <amery@xiangfa.org>
2025-09-24 19:55:15 +0800

    chore: Update model list

--- src/constants/models.ts
+++ src/constants/models.ts
@@ -1,8 +1,10 @@
 export default {
-  openai: "OpenAI GPT-5 Nano",
   "openai-fast": "OpenAI GPT-4.1 Nano",
+  "openai-large": "OpenAI GPT-5",
   "openai-reasoning": "OpenAI o4 Mini",
+  "gemini-search": "Gemini 2.5 Flash",
   gemini: "Gemini 2.5 Flash Lite",
+  deepseek: "DeepSeek V3.1",
   "deepseek-reasoning": "DeepSeek R1",
   mistral: "Mistral Small",
   "nova-fast": "Amazon Nova Micro",
